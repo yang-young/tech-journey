2089ebefb6fa84312ab5497018b24e77067ba42d
YangGuanZhe <yang.guanzhe@tentacles.cn>
2024-11-22 15:22:29 +0800

    add config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://img.carrothub.xyz',
+  url: 'https://md.carrothub.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
